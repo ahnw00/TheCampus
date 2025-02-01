59c7ac9157446a1a268029f31e6018e603be8d9f
ahnw00 <ahnw00@naver.com>
2025-02-02 01:05:19 +0900

    memo limit

--- Assets/Scripts/UI/Record&Memo/DrawLine.cs
+++ Assets/Scripts/UI/Record&Memo/DrawLine.cs
@@ -10,27 +10,34 @@ public class DrawLine : MonoBehaviour
     List<Vector2> points = new List<Vector2>();
     public GameObject curPage;
     private Camera cam;
+    private float leftX, leftY, rightX, rightY;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         cam = GameManager.GameManager_Instance.cam;
+        float midX = Screen.width / 2;
+        float midY = Screen.height / 2;
+        midX += curPage.GetComponent<RectTransform>().anchoredPosition.x;
+        midY += curPage.GetComponent<RectTransform>().anchoredPosition.y;
+        leftX = midX - 649f; leftY = midY - 362f;
+        rightX = midX + 581f; rightY = midY + 362f;
     }
 
     // Update is called once per frame
     void Update()
     {
         Vector2 pos = cam.ScreenToWorldPoint(Input.mousePosition);
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && CheckMousePos(Input.mousePosition))
         {
-            Debug.Log(Input.mousePosition);
+            //Debug.Log(Input.mousePosition);
             GameObject obj = Instantiate(linePrefab, curPage.transform);
             lr = obj.GetComponent<LineRenderer>();
             points.Add(pos);
             lr.positionCount = 1;
             lr.SetPosition(0, points[0]);
         }
-        else if(Input.GetMouseButton(0))
+        else if(Input.GetMouseButton(0) && CheckMousePos(Input.mousePosition))
         {
             if (Vector2.Distance(points[points.Count - 1], pos) > 0.1f)
             {
@@ -43,5 +50,13 @@ public class DrawLine : MonoBehaviour
         {
             points.Clear();
         }
+    }
+
+    bool CheckMousePos(Vector2 mousePos)
+    {
+        if (mousePos.x >= leftX && mousePos.x <= rightX && 
+            mousePos.y >= leftY && mousePos.y <= rightY)
+            return true;
+        return false;
     }
 }
