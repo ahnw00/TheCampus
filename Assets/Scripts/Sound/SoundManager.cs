7713dea4b2890ccb749e58c99191e612ab7aecb6
1window2 <alex1696@naver.com>
2025-02-20 22:33:40 +0900

    start volume
    start volume fix

--- Assets/Scripts/Sound/SoundManager.cs
+++ Assets/Scripts/Sound/SoundManager.cs
@@ -9,18 +9,26 @@ public class SoundManager : MonoBehaviour
     public float BGMVolume { get; set; }
     public float SFXVolume { get; set; }
 
+    private void Start()
+    {
+        // 게임 실행 시 볼륨 무조건 1로 고정되는 문제 때문에; 게임 종료 직전 볼륨 강제적용
+        audioMixer.SetFloat("BGM", Mathf.Log10(Mathf.Clamp(BGMVolume, 0.0001f, 1f)) * 20);
+        audioMixer.SetFloat("SFX", Mathf.Log10(Mathf.Clamp(SFXVolume, 0.0001f, 1f)) * 20);
+    }
+
     private void Awake()
     {
         if (Instance == null)
         {
             Instance = this;
             DontDestroyOnLoad(gameObject);
+            LoadVolume();
         }
         else
         {
             Destroy(this.gameObject);
+            return;
         }
-        LoadVolume();
     }
 
     public void SetBGMValue(float value)
