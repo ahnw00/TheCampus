2dd5f81ccf65410b23a4d7987747ba293beaa3f8
steinsring <lsglsj20@gmail.com>
2025-03-06 22:49:37 +0900

    add text

--- Assets/Scripts/Effect/PrologueController.cs
+++ Assets/Scripts/Effect/PrologueController.cs
@@ -1,3 +1,7 @@
+using NUnit.Framework;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -5,8 +9,16 @@ using UnityEngine.UI;
 public class PrologueController : MonoBehaviour
 {
     [SerializeField] GameObject eyeImage;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] TextMeshProUGUI textMesh;
+    [SerializeField] List<string> newsText = new List<string>();
+    [SerializeField] List<string> speechText = new List<string>();
+    private float typingSpeed = 0.05f;
+    private int index = 0;
 
+    void Start()
+    {
+        index = 0;
+    }
     void Expansion()
     {
 
@@ -15,5 +27,23 @@ public class PrologueController : MonoBehaviour
     void LoadGameScene()
     {
         SceneManager.LoadScene("GameScene");
+    }
+
+
+
+    void SetText()
+    {
+        StartCoroutine(TextTyping());
+    }
+
+    IEnumerator TextTyping()
+    {
+        textMesh.text = "";
+        for (int i = 0; i < speechText[index].Length; i++)
+        {
+            textMesh.text += speechText[i];
+            yield return new WaitForSeconds(typingSpeed);
+        }
+        index++;
     }
 }
