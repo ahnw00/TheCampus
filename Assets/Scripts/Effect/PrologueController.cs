18173539c71c557e12a9004a7e23c73552b34cd6
steinsring <lsglsj20@gmail.com>
2025-03-06 23:30:22 +0900

    text Effect

--- Assets/Scripts/Effect/PrologueController.cs
+++ Assets/Scripts/Effect/PrologueController.cs
@@ -9,15 +9,21 @@ using UnityEngine.UI;
 public class PrologueController : MonoBehaviour
 {
     [SerializeField] GameObject eyeImage;
-    [SerializeField] TextMeshProUGUI textMesh;
+    [SerializeField] TextMeshProUGUI underText;
+    [SerializeField] TextMeshProUGUI centerText;
     [SerializeField] List<string> newsText = new List<string>();
     [SerializeField] List<string> speechText = new List<string>();
     private float typingSpeed = 0.05f;
     private int index = 0;
+    private int newsIndex = 0;
+    private bool isTyping = false; // 타이핑 중인지 체크
 
     void Start()
     {
         index = 0;
+        newsIndex = 0;
+        centerText.text = "";
+        underText.text = "";
     }
     void Expansion()
     {
@@ -31,19 +37,43 @@ public class PrologueController : MonoBehaviour
 
 
 
-    void SetText()
+    void SetTypingText()
     {
-        StartCoroutine(TextTyping());
+        if (!isTyping && index < speechText.Count)
+        {
+            StartCoroutine(TextTyping());
+        }
     }
 
     IEnumerator TextTyping()
     {
-        textMesh.text = "";
-        for (int i = 0; i < speechText[index].Length; i++)
+        isTyping = true;
+        centerText.text = "";
+
+        foreach (char letter in speechText[index]) // 한 글자씩 출력
         {
-            textMesh.text += speechText[i];
+            centerText.text += letter;
             yield return new WaitForSeconds(typingSpeed);
         }
+        centerText.text += "\n";
         index++;
+        foreach (char letter in speechText[index]) // 한 글자씩 출력
+        {
+            centerText.text += letter;
+            yield return new WaitForSeconds(typingSpeed);
+        }
+
+        index++; // 타이핑 완료 후 다음 문장으로 이동
+        isTyping = false;
+    }
+
+    void SetNewsText()
+    {
+        underText.text = newsText[newsIndex++];
+    }
+
+    void SetSpeechText()
+    {
+        centerText.text = speechText[index++];
     }
 }
