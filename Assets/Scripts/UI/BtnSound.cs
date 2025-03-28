43747a37037354e531a27916011bb8ab14aa0852
ahnw00 <ahnw00@naver.com>
2025-03-29 00:51:42 +0900

    btn sound

---
+++ Assets/Scripts/UI/BtnSound.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BtnSound : MonoBehaviour
+{
+    [SerializeField] SoundManager soundManager;
+    [SerializeField] AudioClip btnClip;
+
+    private void OnEnable()
+    {
+        soundManager = FindAnyObjectByType<SoundManager>();
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        this.GetComponent<Button>().onClick.AddListener(PlayBtnSound);
+    }
+
+    void PlayBtnSound()
+    {
+        soundManager.ChangeSfxClip(btnClip);
+    }
+}
