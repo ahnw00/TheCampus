3d90e7cba0fc2261059e5a68b549025e59bcbdbb
1window2 <alex1696@naver.com>
2025-03-28 21:14:13 +0900

    note
    auto size change

---
+++ Assets/Scripts/Item/NoteManager.cs
@@ -0,0 +1,14 @@
+using JetBrains.Annotations;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NoteManager : MonoBehaviour
+{
+    public Image noteImage;
+
+    void Update()
+    {
+        noteImage = GetComponent<Image>();
+        noteImage.SetNativeSize();
+    }
+}
