29c746f44be506a1268d04021f4b1bcc3792c998
steinsring <lsglsj20@gmail.com>
2025-03-29 22:05:05 +0900

    doublecheck update

--- Assets/Scripts/SaveLoad/NewGameBtn.cs
+++ Assets/Scripts/SaveLoad/NewGameBtn.cs
@@ -1,11 +1,14 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class NewGameBtn : MonoBehaviour
 {
     [SerializeField] DataManager dataManager;
     [SerializeField] SaveDataClass saveData;
     [SerializeField] GameObject loadingImage;
+    [SerializeField] Button continuebtn;
+    [SerializeField] GameObject doublecheck;
 
     [SerializeField] GameObject mainScene;
     [SerializeField] Animator mainStory;
@@ -18,6 +21,22 @@ public class NewGameBtn : MonoBehaviour
     }
 
     public void OnNewGameClicked()
+    {
+        if (continuebtn.interactable)
+        {
+            FindAnyObjectByType<SoundManager>().ChangeBgmClip(null);
+            dataManager.DataInitialize();
+            Debug.Log(saveData.isNew);
+            mainScene.SetActive(true);
+            mainStory.Play("MainScene");
+        }
+        else
+        {
+            doublecheck.SetActive(true);
+        }
+    }
+
+    public void OnDoublecheckClicked()
     {
         FindAnyObjectByType<SoundManager>().ChangeBgmClip(null);
         dataManager.DataInitialize();
